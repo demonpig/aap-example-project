e9461eadfa9db25db71808dd312e780b02c2b9eb
Max Mitschke <maxmitschke@fastmail.com>
2023-09-09 18:03:34 -0400

    Create dynamic_inventory.py

---
+++ inventories/dynamic_inventory.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import json
+
+count = 50000
+
+def _generate_inventory():
+
+    ungrouped = ["host_{}.local".format(i) for i in range(count)]
+
+    hostvars = {}
+    for i in range(count):
+        name = "host_{}.local".format(i)
+        hostvars[name] = {'ansible_host': '', 'ansible_connection': ''}
+        hostvars[name]['ansible_host'] = '127.0.0.1'
+        hostvars[name]['ansible_connection'] = 'local' 
+
+    data = {
+        '_meta': {
+            'hostvars': hostvars
+        },
+        'all': {
+            'children': ['ungrouped']
+        },
+        'ungrouped': {
+            'hosts': ungrouped
+        }
+    }
+
+    return json.dumps(data)
+
+if __name__ == '__main__':
+    print(_generate_inventory())
