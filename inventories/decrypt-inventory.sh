41806dc05edbc18ab1aa5fc3bd4e10f62b9da170
Max Mitschke <maxmitschke@fastmail.com>
2024-07-16 11:40:48 -0400

    Update decrypt-inventory.sh

--- inventories/decrypt-inventory.sh
+++ inventories/decrypt-inventory.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ls $CUSTOM_VAULT_INVENTORY > /dev/null || { echo "Inventory not found" ; exit 2; }
+ls "$(dirname $0)/$CUSTOM_VAULT_INVENTORY" > /dev/null || { echo "Inventory not found" ; exit 2; }
 
 ansible-inventory --list --export --vault-password-file ${CUSTOM_VAULT_PASSWORD_FILE:-""} -i "$(dirname $0)/$CUSTOM_VAULT_INVENTORY"
