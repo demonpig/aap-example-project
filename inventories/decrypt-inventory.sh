06e913bf5ebf3a0b8b12d72f44925bb9082f00e9
Max Mitschke <maxmitschke@fastmail.com>
2024-07-16 10:42:22 -0400

    Update decrypt-inventory.sh

--- inventories/decrypt-inventory.sh
+++ inventories/decrypt-inventory.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-ansible-vault view --vault-password-file=${FOOBAR:-""} "$(dirname $0)/encrypted-inventory.yml"
+TEMP_FILE="$RANDOM"
+
+ansible-vault view --vault-password-file=${FOOBAR:-""} "$(dirname $0)/${ENCRYPTED_FILENAME:-encrypted-inventory.yml}" > $TEMP_FILE
+
+ansible-inventory --list --export -i $TEMP_FILE
+
+rm -f $TEMP_FILE
