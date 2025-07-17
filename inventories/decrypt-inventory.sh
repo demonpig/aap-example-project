94e669177fdd6bf97a02c7b5e2d25109e053f5de
Max Mitschke <mmitschk@redhat.com>
2025-07-17 15:53:27 -0400

    Removing a lot of data

--- inventories/decrypt-inventory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-INV_FILENAME="$(dirname $0)/${CUSTOM_VAULT_INVENTORY}"
-
-test -f "${INV_FILENAME}" || { echo "Inventory not found"; exit 2; }
-test -f "${CUSTOM_VAULT_PASSWORD_FILE}" || { echo "ansible-vault password file not found"; exit 3; }
-
-ansible-inventory --list --export --vault-password-file ${CUSTOM_VAULT_PASSWORD_FILE} -i "${INV_FILENAME}"
--- inventories/dynamic_inventory_bash.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-PYTHON_BIN="python3"
-
-command -v $PYTHON_BIN > /dev/null || exit 100
-
-$PYTHON_BIN "$(dirname $0)/dynamic_inventory.py"
